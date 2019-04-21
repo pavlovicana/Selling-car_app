6d1763c601096a00493105cd2640a6554e531012
Ana Pavlovic <49585841+pavlovicana@users.noreply.github.com>
2019-04-21 14:03:32 +0200

    initial commit

---
+++ ConsoleApplication22/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Car
+    {
+        internal string owner;
+        internal  string Owner
+        {
+            get { return owner; }
+            set { owner = value; }
+        }
+
+        internal Car(string owner)
+        {
+            Owner = owner;
+        }
+        internal void ShowDetails()
+        {
+            Console.WriteLine("First owner: {0}", owner);
+        }
+
+        internal void SellCar(string newOwner)
+        {
+         
+            if (newOwner != owner)
+            {
+                Console.WriteLine("New owner is: {0}", newOwner);
+            }
+        }
+          
+    }
+}
---
+++ ConsoleApplication22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car firstCar = new Car("AUDI fabrika");
+            firstCar.ShowDetails();
+           
+            firstCar.SellCar("Ana Pavlovic");
+       
+           
+            Console.ReadLine();
+
+
+        }
+    }
+}
